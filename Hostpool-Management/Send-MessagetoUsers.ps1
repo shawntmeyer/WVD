12219005e6e6aef84f2748a1f46e41d5eeed182e
Shawn Meyer <shmeyer@microsoft.com>
2020-10-24 21:27:44 -0400

    updated send-messagetousers.ps1 script by putting function first and optimizing the write-output text.

--- Hostpool-Management/Send-MessagetoUsers.ps1
+++ Hostpool-Management/Send-MessagetoUsers.ps1
@@ -43,45 +43,6 @@ param
     [Parameter(Mandatory)]
     [string] $MessageBody
 )
-$HostPool = $null
-try {
-    Write-Output "Get Hostpool info: [$HostPoolName] in resource group: [$ResourceGroupName]."
-    $HostPool = Get-AzWvdHostPool -Name $HostPoolName -ResourceGroupName $ResourceGroupName
-    if (-not $HostPool) {
-        throw $HostPool
-    }
-}
-catch {
-    Write-Warning "Hostpoolname [$HostpoolName] does not exist in the [$ResourceGroupName] resource group. Ensure that you have entered the correct values."
-    exit
-}
-
-If ($SessionHostName) {
-    $SessionHost = $null
-    Try {
-        $SessionHost = Get-AzWvdSessionHost -HostPoolName $HostPoolName -ResourceGroupName $ResourceGroupName | Where-Object { $_.SessionHostName -eq $SessionHostName }
-        If (-not $SessionHost) {
-            throw $SessionHost
-        }
-        Send-MessagetoActiveWVDUsers -SessionHostName $SessionHostName -HostpoolName $HostPoolName -ResourceGroupName $ResourceGroupName -MessageTitle $MessageTitle -MessageBody $MessageBody
-    }
-    Catch {
-        Write-Warning "SessionHost [$SessionHostName] does not exist in the [$HostPoolName] hostpool in the [$ResourceGroupName] resource group."
-        exit
-    } 
-}
-Else {
-    $SessionHosts = Get-AzWvdSessionHost -HostPoolName $HostpoolName -ResourceGroupName $ResourceGroupName -ErrorAction Stop | Sort-Object Name
-    if (-not $SessionHosts) {
-        Write-Warning "There are no session hosts in the [$HostpoolName] hostpool."
-        exit
-    }
-    
-    ForEach ($SessionHost in $SessionHosts) {
-        $SessionHostName = $SessionHost.Name.Split("/")[1]
-        Send-MessagetoActiveWVDUsers -SessionHostName $SessionHostName -HostpoolName $HostPoolName -ResourceGroupName $ResourceGroupName -MessageTitle $MessageTitle -MessageBody $MessageBody
-    }
-}
 
 Function Send-MessagetoActiveWVDUsers {
     [CmdletBinding()]
@@ -102,23 +63,70 @@ Function Send-MessagetoActiveWVDUsers {
         [string] $MessageBody
 
     )
+    Write-Output "------------------------------------------------------"
+    Write-Output "[$SessionHostName]: Checking for active user sessions."
+
     $ActiveUserSessions = Get-AzWvdUserSession -HostPoolName $HostpoolName -ResourceGroupName $ResourceGroupName -SessionHostName $SessionHostName | Where-Object {$_.SessionState -eq 'Active'}
     if (($ActiveUserSessions).Count -gt 0) {
-        Write-Output "Sending console message to active users on [$SessionHostName]."
+        Write-Output "[$SessionHostName]: There are active user sessions. Sending console message to all active users."
         Foreach ($Session in $ActiveUserSessions) {
             $SplitSessionID = $Session.Id.Split("/")
             $SessionID = $SplitSessionID[$SplitSessionID.Count - 1]
             $UserName = $Session.ActiveDirectoryUserName
-            Write-Output "Sending message to [$UserName]."
+            Write-Output "[$SessionHostName]: Sending message to [$UserName]."
             try {
                 Send-AzWvdUserSessionMessage -ResourceGroupName $ResourceGroupName -HostPoolName $HostPoolName -SessionHostName $SessionHostName -UserSessionId $SessionID -MessageTitle $MessageTitle -MessageBody $MessageBody
+                Write-output "[$SessionHostName]: Message sent to [$UserName] successfully."
             }
             Catch {
-                Write-Warning "Failed to send message to user: '$($UserName)', session ID: $SessionID $($PSItem | Format-List -Force | Out-String)"
+                Write-Warning "[$SessionHostName]: Failed to send message to user: '$($UserName)', session ID: $SessionID $($PSItem | Format-List -Force | Out-String)"
             }
         }
     }
     Else {
-        Write-Output "There are no active user sessions on any session hosts in the [$HostpoolName] hostpool."
+        Write-Output "[$SessionHostName]: There are no active user sessions on this session host. No messages sent."
+    }
+}
+
+$HostPool = $null
+try {
+    Write-Output "Verification: Hostpool: [$HostPoolName] exists in resource group: [$ResourceGroupName]."
+    $HostPool = Get-AzWvdHostPool -Name $HostPoolName -ResourceGroupName $ResourceGroupName
+    if (-not $HostPool) {
+        throw $HostPool
+    }
+    Write-Output "Verification: Hostpool information verified."
+}
+catch {
+    Write-Warning "Hostpool: [$HostpoolName] does not exist in the resource group: [$ResourceGroupName]. Ensure that you have entered the correct values."
+    exit
+}
+
+If ($SessionHostName) {
+    $SessionHost = $null
+    Try {
+        Write-Output "Verification: Session host: [$SessionHostName] exists in hostpool: [$HostpoolName]."
+        $SessionHost = Get-AzWvdSessionHost -HostPoolName $HostPoolName -ResourceGroupName $ResourceGroupName | Where-Object { $_.Name -eq "$Hostpoolname/$SessionHostName" }
+        If (-not $SessionHost) {
+            throw $SessionHost
+        }
+        Send-MessagetoActiveWVDUsers -SessionHostName $SessionHostName -HostpoolName $HostPoolName -ResourceGroupName $ResourceGroupName -MessageTitle $MessageTitle -MessageBody $MessageBody
+    }
+    Catch {
+        Write-Warning "SessionHost: [$SessionHostName] does not exist in the hostpool: [$HostPoolName] in the resource group: [$ResourceGroupName]."
+        exit
+    } 
+}
+Else {
+    Write-Output "[$HostpoolName]: Retrieving list of session hosts in hostpool."
+    $SessionHosts = Get-AzWvdSessionHost -HostPoolName $HostpoolName -ResourceGroupName $ResourceGroupName -ErrorAction Stop | Sort-Object Name
+    if (-not $SessionHosts) {
+        Write-Warning "[$HostpoolName]: There are no session hosts in this hostpool."
+        exit
+    }
+    
+    ForEach ($SessionHost in $SessionHosts) {
+        $SessionHostName = $SessionHost.Name.Split("/")[1]
+        Send-MessagetoActiveWVDUsers -SessionHostName $SessionHostName -HostpoolName $HostPoolName -ResourceGroupName $ResourceGroupName -MessageTitle $MessageTitle -MessageBody $MessageBody
     }
 }
