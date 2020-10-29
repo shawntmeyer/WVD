dbae7115d50968fad716b045d3da0a6013164f5f
Shawn Meyer <shmeyer@microsoft.com>
2020-10-28 20:33:48 -0400

    Added lines to pull the WVD Optimization script and run it.

--- Image-Build/AIB/Invoke-AIBImageCustomization.ps1
+++ Image-Build/AIB/Invoke-AIBImageCustomization.ps1
@@ -1,7 +1,18 @@
 $BuildDir = "c:\BuildDir"
 New-Item -Path "$BuildDir" -ItemType Directory -Force -ErrorAction SilentlyContinue
-Invoke-WebRequest -Uri "https://github.com/shawntmeyer/WVD/archive/master.zip" -outfile "$BuildDir\WVD-Master.zip" -UseBasicParsing
-Expand-Archive -Path "$BuildDir\WVD-Master.zip" -DestinationPath "$BuildDir"
-& "$BuildDir\WVD-Master\Image-Build\Customizations\Prepare-WVDImage.ps1"
-Remove-Item -Path "$BuildDir" -Recurse -ErrorAction SilentlyContinue
-Get-ChildItem -Path $env:Temp -Recurse -ErrorAction SilentlyContinue | Remove-Item -ErrorAction SilentlyContinue
+$PrepWVDImageURL = "https://github.com/shawntmeyer/WVD/archive/master.zip"
+$PrepareWVDImageZip= "$BuildDir\WVD-Master.zip"
+Invoke-WebRequest -Uri $PrepWVDImageURL -outfile $PrepareWVDImageZip -UseBasicParsing
+Expand-Archive -Path $PrepareWVDImageZip -DestinationPath $BuildDir
+$ScriptPath = "$BuildDir\WVD-Master\Image-Build\Customizations"
+Set-Location -Path $ScriptPath
+.\Prepare-WVDImage.ps1 -RemoveApps $False
+$WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
+$WVDOptimizeZIP = "$BuildDir\Windows_10_VDI_Optimize-master.zip"
+Invoke-WebRequest -Uri $WVDOptimizeURL -OutFile $WVDOptimizeZIP -UseBasicParsing
+Expand-Archive -Path $WVDOptimizeZIP -DestinationPath $BuildDir -force
+$ScriptPath = "$BuildDir\Virtual-Desktop-Optimization-Tool-master"
+Set-Location -Path $ScriptPath
+.\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Verbose
+Set-Location -Path c:\
+Remove-Item -Path $BuildDir -Recurse -Force -ErrorAction SilentlyContinue
