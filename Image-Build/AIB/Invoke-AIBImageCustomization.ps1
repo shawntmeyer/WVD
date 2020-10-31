224f6da1ab72a684b27fcbef25b2e2e98bdceb4b
Shawn Meyer <shmeyer@microsoft.com>
2020-10-30 20:12:51 -0400

    moved the Virtual Desktop Optimization Script to a Setupcomplete.cmd action.

--- Image-Build/AIB/Invoke-AIBImageCustomization.ps1
+++ Image-Build/AIB/Invoke-AIBImageCustomization.ps1
@@ -1,3 +1,4 @@
+$WindowsVersion = "2004"
 $BuildDir = "c:\BuildDir"
 New-Item -Path "$BuildDir" -ItemType Directory -Force -ErrorAction SilentlyContinue
 $PrepWVDImageURL = "https://github.com/shawntmeyer/WVD/archive/master.zip"
@@ -7,14 +8,32 @@ Expand-Archive -Path $PrepareWVDImageZip -DestinationPath $BuildDir
 $ScriptPath = "$BuildDir\WVD-Master\Image-Build\Customizations"
 Set-Location -Path $ScriptPath
 .\Prepare-WVDImage.ps1 -RemoveApps $False
-$WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
-$WVDOptimizeZIP = "$BuildDir\Windows_10_VDI_Optimize-master.zip"
-Invoke-WebRequest -Uri $WVDOptimizeURL -OutFile $WVDOptimizeZIP -UseBasicParsing
-Expand-Archive -Path $WVDOptimizeZIP -DestinationPath $BuildDir -force
-$ScriptPath = "$BuildDir\Virtual-Desktop-Optimization-Tool-master"
-Set-Location -Path $ScriptPath
-$Script = "Win10_VirtualDesktop_Optimize.ps1"
-(Get-Content "$ScriptPath\$Script").replace('$Answer = [System.Windows.MessageBox]::Show("Reboot to make changes effective?", "Restart Computer", "YesNo", "Question")', '$Answer = "No"') | Set-Content "$ScriptPath\$Script"
-.\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2004 -Verbose
-Set-Location -Path c:\
 Remove-Item -Path $BuildDir -Recurse -Force -ErrorAction SilentlyContinue
+
+# Create the default setup scripts directory if it doesn't exist. If setupcomplete.cmd is found in this directory by Windows Setup it is ran
+# after Windows Setup finishes but before the user interface is displayed. See https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/add-a-custom-script-to-windows-setup
+$SetupDir = "$env:SystemRoot\Setup\Scripts"
+If (!(Test-Path $SetupDir)) {
+    $null = New-Item -Name $SetupDir -ItemType Directory
+}
+#Download Virtual Desktop Optimization Tool from the Virtual Desktop Team GitHub Repo
+$WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
+$WVDOptimizeZIP = "$SetupDir\Windows_10_VDI_Optimize-master.zip"
+Invoke-WebRequest -Uri $WVDOptimizeURL -OutFile $WVDOptimizeZIP -UseBasicParsing
+Expand-Archive -Path $WVDOptimizeZIP -DestinationPath $SetupDir -force
+$ScriptPath = "$SetupDir\Virtual-Desktop-Optimization-Tool-master"
+# Update the script configuration to leave the windows calculator enabled.
+$AppxPackagesConfigFileFullName = "$scriptPath\$WindowsVersion\ConfigurationFiles\AppxPackages.json"
+
+$AppxPackagesObj = Get-Content "$AppxPackagesConfigFileFullName" -Raw | ConvertFrom-Json
+ForEach ($Element in $AppxPackagesObj) {
+    If ($Element.AppxPackage -eq 'Microsoft.WindowsCalculator') {
+        $Element.VDIState = 'Enabled'
+    }
+}
+$AppxPackagesObj | ConvertTo-Json -depth 32 | Set-Content $AppxPackagesConfigFileFullName
+# Add a new setupcomplete.cmd if it is needed else just add a new line to an existing one to call the Win10_VirtualDesktop_Optimization.ps1 script during deployment.
+If (!(Test-Path $SetupDir\setupcomplete.cmd)){
+    New-Item -Path $SetupDir\setupcomplete.cmd -ItemType File
+}
+Add-Content -Path $SetupDir\setupcomplete.cmd -Value "Powershell.exe -executionpolicy bypass -file `"$ScriptPath\Win10_VirtualDesktop_Optimize.ps1`" -WindowsVersion $WindowsVersion -Restart -Verbose"
