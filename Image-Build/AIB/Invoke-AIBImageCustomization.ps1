ab0d698e5f72acbcfa22b416282d4cf4d0b2a6bd
Shawn Meyer <shmeyer@microsoft.com>
2020-10-31 21:16:56 -0400

    changed code to run optimization tool in image.

--- Image-Build/AIB/Invoke-AIBImageCustomization.ps1
+++ Image-Build/AIB/Invoke-AIBImageCustomization.ps1
@@ -4,6 +4,7 @@ $ScriptName = $MyInvocation.MyCommand.Name
 Write-Output "Running '$ScriptName'"
 Write-Output "Creating '$BuildDir'"
 $null = New-Item -Path "$BuildDir" -ItemType Directory -Force -ErrorAction SilentlyContinue
+Write-Output "Downloading the WVD Image Prep Script from the 'http://www.github.com/shawntmeyer/wvd' repo."
 $PrepWVDImageURL = "https://github.com/shawntmeyer/WVD/archive/master.zip"
 $PrepareWVDImageZip= "$BuildDir\WVD-Master.zip"
 Write-Output "Downloading '$PrepWVDImageURL' to '$PrepareWVDImageZip'."
@@ -13,23 +14,7 @@ $ScriptPath = "$BuildDir\WVD-Master\Image-Build\Customizations"
 Set-Location -Path $ScriptPath
 Write-Output "Running Prepare-WVDImage.ps1"
 .\Prepare-WVDImage.ps1 -RemoveApps $False
-Set-Location "$env:SystemDrive"
-Write-Output "Removing '$BuildDir'."
-Remove-Item -Path $BuildDir\* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item -Path $BuildDir -Recurse -Force -ErrorAction SilentlyContinue
-
-<##
-    OOBE supports running a custom script after setup completes named C:\Windows\Setup\Scripts\SetupComplete.cmd
-    (see https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/add-a-custom-script-to-windows-setup).
-    However, Azure's provisioning process uses this script (overwriting if necessary) to bootstrap its own
-    OOBE process. Luckily, Azure's OOBE process leaves a hook for us at the end of its process by running the script
-    C:\Windows\OEM\SetupComplete2.cmd, if present.
-##>
-$OEMDir = "$env:SystemRoot\OEM"
-If (!(Test-Path $OEMDir)) {
-    Write-Output "Creating '$OEMDir'."
-    $null = New-Item -Name OEM -Path $env:SystemRoot -ItemType Directory -Force
-}
+Write-Output "Finished 'Prepare-WVDImage.ps1'."
 # Download Virtual Desktop Optimization Tool from the Virtual Desktop Team GitHub Repo
 $WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/master.zip'
 $WVDOptimizeZIP = "$OEMDir\Windows_10_VDI_Optimize-master.zip"
@@ -38,9 +23,9 @@ Write-Output "Downloading '$WVDOptimizeURL' to '$WVDOptimizeZIP'."
 Invoke-WebRequest -Uri $WVDOptimizeURL -OutFile $WVDOptimizeZIP -UseBasicParsing
 Expand-Archive -Path $WVDOptimizeZIP -DestinationPath $OEMDir -force
 Remove-Item -Path $WVDOptimizeZIP -Force -ErrorAction SilentlyContinue
-$ScriptPath = "$OEMDir\Virtual-Desktop-Optimization-Tool-master"
+$ScriptPath = "$BuildDir\Virtual-Desktop-Optimization-Tool-master"
 # Update the optimization script's configuration to keep the windows calculator app.
-Write-Output "Staging the VD Optimization Tool at '$ScriptPath'."
+Write-Output "Staging the Virtual Desktop Optimization Tool at '$ScriptPath'."
 Write-Output "Changing AppPackages.json file to leave WindowsCalculator app."
 $AppxPackagesConfigFileFullName = "$scriptPath\$WindowsVersion\ConfigurationFiles\AppxPackages.json"
 $AppxPackagesObj = Get-Content "$AppxPackagesConfigFileFullName" -Raw | ConvertFrom-Json
@@ -50,9 +35,26 @@ ForEach ($Element in $AppxPackagesObj) {
     }
 }
 $AppxPackagesObj | ConvertTo-Json -depth 32 | Set-Content $AppxPackagesConfigFileFullName
+$WVDOptimizeScriptName = (Get-ChildItem $ScriptPath | Where-Object {$_.Name -like '*optimize*.ps1'}).Name
+Write-Output "Adding a -NoRestart parameter to the Set-NetAdapterAdvancedProperty line in '$WVDOptimizeScriptName' to prevent the network adapter restart from killing AIB."
+$WVDOptScriptFile = Join-Path -Path $ScriptPath -ChildPath $WVDOptimizeScriptName
+(Get-Content $WVDOptScriptFile) | ForEach-Object { if (($_ -like 'Set-NetAdapterAdvancedProperty*') -and ($_ -notlike '*-NoRestart*')) { $_ -replace "$_", "$_ -NoRestart" } else { $_ } } | Set-Content $WVDOptScriptFile
+Set-Location $ScriptPath
+Write-Output "Now calling '$WVDOptimizeScriptName'."
+.\$WVDOptimizeScriptName -WindowsVersion $WindowsVersion -Verbose
+Write-Output 'Cleaning up from customization scripts.'
+Set-Location "$env:SystemDrive"
+Write-Output "Removing '$BuildDir'."
+Remove-Item -Path $BuildDir\* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path $BuildDir -Recurse -Force -ErrorAction SilentlyContinue
+
+
+<##
+
 Write-Output "Creating '$OEMDir\setupcomplete2.cmd' and adding command to run $ScriptPath\Win10_VirtualDesktop_Optimize.ps1 post Windows Setup."
 $CMDLine = "Powershell.exe -noprofile -noninteractive -executionpolicy bypass -file `"$ScriptPath\Win10_VirtualDesktop_Optimize.ps1`" -WindowsVersion $WindowsVersion -Verbose > %windir%\oem\win10_virtualdesktop_optimize.log"
 $CMDLine | out-file -Encoding ascii -FilePath "$OEMDir\setupcomplete2.cmd"
 # Create the following tag file to force a machine restart from c:\windows\oem\setupcomplete.cmd
 Write-Output "Adding the 'RestartMachine.tag' file to the '$OEMDir'."
 $Null = New-Item -Path "$OEMDir" -Name "RestartMachine.tag" -ItemType File
+##>
